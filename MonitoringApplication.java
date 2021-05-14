285a2da60de9366346a76248d72b7f7676730438
rawaljaishankar <rawaljaishankar@gmail.com>
2021-05-14 11:11:14 +0530

    Add files via upload

---
+++ MonitoringApplication.java
@@ -0,0 +1,13 @@
+package sgcib.solstis.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringApplication.class, args);
+	}
+
+}
